a8d8511a1a98c1abac99b24232673c523a9fcf1c
Tom McLaughlin <pyro777@gmail.com>
2018-09-04 11:48:37 -0700

    Add more HasCallStack

--- Test/Hspec/WebDriver/Core.hs
+++ Test/Hspec/WebDriver/Core.hs
@@ -1,5 +1,5 @@
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
-{-# LANGUAGE OverloadedStrings, QuasiQuotes, FlexibleInstances, DeriveDataTypeable, TypeFamilies, CPP, NamedFieldPuns, ScopedTypeVariables, TupleSections #-}
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, FlexibleInstances, DeriveDataTypeable, TypeFamilies, CPP, NamedFieldPuns, ScopedTypeVariables, TupleSections, InstanceSigs #-}
 module Test.Hspec.WebDriver.Core where
 
 import Control.Concurrent
@@ -8,9 +8,10 @@ import Control.Exception.Lifted (try, onException, throwIO)
 import Control.Monad
 import Data.IORef (newIORef, writeIORef, readIORef)
 import Data.Typeable (cast)
+import GHC.Stack
 import Test.Hspec
 import qualified Test.Hspec as H
-import Test.Hspec.Core.Spec (Result(..), Example(..), Item(..), fromSpecList, runSpecM)
+import Test.Hspec.Core.Spec (Result(..), Example(..), Item(..), fromSpecList, runSpecM, Params, ProgressCallback)
 import Test.Hspec.WebDriver.Types
 import Test.Hspec.WebDriver.Util
 import qualified Test.WebDriver as W
@@ -21,6 +22,8 @@ import qualified Test.WebDriver.Session as W
 
 instance Eq multi => Example (WdExample multi) where
   type Arg (WdExample multi) = WdTestSession multi
+
+  evaluateExample :: (HasCallStack) => WdExample multi -> Params -> (ActionWith (Arg (WdExample multi)) -> IO ()) -> ProgressCallback -> IO Result
   evaluateExample (WdPending msg) _ _ _ = return $ Pending msg
   evaluateExample wdExample _ act _ = do
     -- IORefs are gross here but they're necessary to get the results out of 'act'
@@ -41,7 +44,7 @@ instance Eq multi => Example (WdExample multi) where
         (_, True) -> Pending (Just "Session has been aborted")
         _ -> Success
 
-runAction' :: Eq multi => WdExample multi -> WdTestSession multi -> IO (WdTestSession multi, Maybe SomeException, Bool)
+runAction' :: (HasCallStack, Eq multi) => WdExample multi -> WdTestSession multi -> IO (WdTestSession multi, Maybe SomeException, Bool)
 runAction' (WdPending _) _ = error "runAction called on a WdPending"
 runAction' (WdExample multi (WdOptions {skipRemainingTestsAfterFailure}) wd) tstate = do
   let skip = (stPrevHadError tstate || stPrevAborted tstate) && skipRemainingTestsAfterFailure
@@ -74,7 +77,7 @@ runAction' (WdExample multi (WdOptions {skipRemainingTestsAfterFailure}) wd) tst
 
   return (tstate', maybeError, skip)
 
-createWDSession :: W.WDConfig -> IO W.WDSession
+createWDSession :: (HasCallStack) => W.WDConfig -> IO W.WDSession
 createWDSession cfg = do
   s <- W.mkSession cfg
 #if MIN_VERSION_webdriver(0,7,0)
@@ -90,14 +93,14 @@ createWDSession cfg = do
 --------------------------------------------------------------------------------
 
 -- | Convert a single test item to a generic item by providing it with the WdTestSession.
-procSpecItem :: t -> Item t -> Item ()
+procSpecItem :: (HasCallStack) => t -> Item t -> Item ()
 procSpecItem sessionState item = item { itemExample = \p act progress -> itemExample item p (act . act') progress }
   where act' f () = f sessionState
 
 -- | Convert a spec tree of test items to a spec tree of generic items by creating a single session for
 -- the entire tree.
 -- Close all sessions at the end of the tests.
-procTestSession :: W.WDConfig -> W.Capabilities -> SpecWith (WdTestSession multi) -> Spec
+procTestSession :: (HasCallStack) => W.WDConfig -> W.Capabilities -> SpecWith (WdTestSession multi) -> Spec
 procTestSession cfg cap spec = do
   sessionsVar <- runIO $ newMVar []
 
